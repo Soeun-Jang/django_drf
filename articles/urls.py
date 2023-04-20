ad0d3e676b1f788750ad797d56979c627d0fa940
Soeun_Jang <azulcielo_@naver.com>
2023-04-20 14:56:47 +0900

    articles/views.py add  method PUT, DELETE

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path,include
+from django.urls import path
 from articles import views
 
 urlpatterns = [
-    path('index/', views.index_view, name="index"),
+    path('', views.article_API, name="index"),
+    path("<int:article_id>/", views.article_view, name="article_view"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,12 +1,13 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from articles.models import Article
 from articles.serializers import ArticleSerializer
 
-
+#해당함수가 지정한 HTTP요청을 처리할 수 있도록 API뷰를 만들어 주는 역할
 @api_view(['GET','POST'])
-def index_view(request):
+def article_API(request):
   if request.method == 'GET':
     articles = Article.objects.all()
     serializer = ArticleSerializer(articles, many=True)
@@ -19,3 +20,20 @@ def index_view(request):
     else:
       print(serializer.errors)
       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET','PUT','DELETE'])
+def article_view(request, article_id):
+  if request.method == 'GET':
+    article = get_object_or_404(Article, id=article_id)
+    serializer = ArticleSerializer(article)
+    return Response(serializer.data)
+  elif request.method == 'PUT':
+    article = get_object_or_404(Article, id=article_id)
+    serializer = ArticleSerializer(article, data=request.data) #(원래 data, 새로운 data) 
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+  elif request.method == 'DELETE':
+    article = get_object_or_404(Article, id=article_id)
+    article.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
