2ae336c0874d98c929d1be7f6dd193b24aebfb64
Soeun_Jang <azulcielo_@naver.com>
2023-04-20 17:05:16 +0900

    FBV를 CBV로 변환

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from articles import views
 
+
 urlpatterns = [
-    path('', views.article_API, name="index"),
-    path("<int:article_id>/", views.article_view, name="article_view"),
+    path('', views.ArticleList.as_view(), name="index"),
+    path("<int:article_id>/", views.ArticleDetail.as_view(), name="article_view"),
 ]
